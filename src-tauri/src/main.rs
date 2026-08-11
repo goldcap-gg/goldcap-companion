4e68e2bf678e43bc614403fa60e48ad042075332
hitfm00 <hitfm00@gmail.com>
2026-08-11 19:50:39 +0300

    feat(companion): wire up tauri-plugin-updater (config + registration)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -28,6 +28,7 @@ fn main() {
         ))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             commands::get_config,
             commands::save_config,
